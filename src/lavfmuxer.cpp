0c901ed0d7512235c6b078ebb306c37a1aa99d70
Martin Doucha <next_ghost@quick.cz>
2012-09-22 15:38:53 +0200

    Fix FFmpeg deprecation warnings

--- src/lavfmuxer.cpp
+++ src/lavfmuxer.cpp
@@ -21,6 +21,7 @@
 extern "C" {
 #include <libavformat/avformat.h>
 #include <libavcodec/avcodec.h>
+#include <libavutil/opt.h>
 }
 #include <cstring>
 #include <utility>
@@ -45,6 +46,17 @@ lavfmuxer::lavfmuxer(const char *format, uint32_t audiostreammask, mpgfile &mpg,
     return;
     }
 
+#if LIBAVFORMAT_VERSION_INT >= ((53<<16)+(0<<8)+0)
+	int ret = avformat_alloc_output_context2(&avfc, fmt, format, filename);
+
+	if (ret < 0) {
+		return;
+	}
+
+	avfc->max_delay = (int)(.7 * AV_TIME_BASE);
+	av_opt_set_int(avfc, "preload", (int)(.5 * AV_TIME_BASE), AV_OPT_SEARCH_CHILDREN);
+	av_opt_set_int(avfc, "muxrate", 10080000, AV_OPT_SEARCH_CHILDREN);
+#else
   avfc=av_alloc_format_context();
   if (!avfc)
     return;
@@ -55,11 +67,17 @@ lavfmuxer::lavfmuxer(const char *format, uint32_t audiostreammask, mpgfile &mpg,
 
   avfc->oformat=fmt;
   strncpy(avfc->filename, filename, sizeof(avfc->filename));
+#endif
 
   int id=0;
 
   st[VIDEOSTREAM].stream_index=id;
+#if LIBAVFORMAT_VERSION_INT >= ((53<<16)+(0<<8)+0)
+	AVStream *s = st[VIDEOSTREAM].avstr = avformat_new_stream(avfc, NULL);
+	s->id = id++;
+#else
   AVStream *s=st[VIDEOSTREAM].avstr=av_new_stream(avfc,id++);
+#endif
   strpres[VIDEOSTREAM]=true;
   av_free(s->codec);
   mpg.setvideoencodingparameters();
@@ -73,12 +91,21 @@ lavfmuxer::lavfmuxer(const char *format, uint32_t audiostreammask, mpgfile &mpg,
     if (audiostreammask & (1u<<i)) {
       int astr=audiostream(i);
       st[astr].stream_index=id;
+#if LIBAVFORMAT_VERSION_INT >= ((53<<16)+(0<<8)+0)
+		s = st[astr].avstr = avformat_new_stream(avfc, NULL);
+		s->id = id++;
+#else
       s=st[astr].avstr=av_new_stream(avfc,id++);
+#endif
       strpres[astr]=true;
       if (s->codec)
         av_free(s->codec);
+#if LIBAVCODEC_VERSION_INT >= ((53<<16)+(0<<8)+0)
+		s->codec = avcodec_alloc_context3(NULL);
+#else
       s->codec = avcodec_alloc_context();
       avcodec_get_context_defaults(s->codec);
+#endif
       s->codec->codec_type=CODEC_TYPE_AUDIO;
       s->codec->codec_id = (mpg.getstreamtype(astr)==streamtype::ac3audio) ?
 	CODEC_ID_AC3 : CODEC_ID_MP2;
@@ -93,17 +120,21 @@ lavfmuxer::lavfmuxer(const char *format, uint32_t audiostreammask, mpgfile &mpg,
 	  break;
 
 	if (sd->getitemlistsize() > 1) {
-	  if (!avcodec_open(s->codec,
-			    avcodec_find_decoder(s->codec->codec_id))) {
+#if LIBAVCODEC_VERSION_INT >= ((53<<16)+(0<<8)+0)
+	  if (!avcodec_open2(s->codec, avcodec_find_decoder(s->codec->codec_id), NULL)) {
+#else
+	  if (!avcodec_open(s->codec, avcodec_find_decoder(s->codec->codec_id))) {
+#endif
 	    int16_t samples[AVCODEC_MAX_AUDIO_FRAME_SIZE/sizeof(int16_t)];
 	    int frame_size=sizeof(samples);
 	    //fprintf(stderr, "** decode audio size=%d\n", sd->inbytes());
 #if LIBAVCODEC_VERSION_INT >= ((53<<16)+(0<<8)+0)
 		AVPacket packet;
+		AVFrame frame;
 		av_init_packet(&packet);
 		packet.data = (uint8_t*)sd->getdata();
 		packet.size = sd->inbytes();
-		avcodec_decode_audio3(s->codec, samples, &frame_size, &packet);
+		avcodec_decode_audio4(s->codec, &frame, &frame_size, &packet);
 #elif LIBAVCODEC_VERSION_INT >= ((52<<16)+(0<<8)+0)
 	    avcodec_decode_audio2
 	      (s->codec,samples,&frame_size,
@@ -120,6 +151,20 @@ lavfmuxer::lavfmuxer(const char *format, uint32_t audiostreammask, mpgfile &mpg,
       }
     }
 
+#if LIBAVFORMAT_VERSION_INT >= ((53<<16)+(0<<8)+0)
+	if (!(fmt->flags & AVFMT_NOFILE) && (avio_open(&avfc->pb, filename, AVIO_FLAG_WRITE) < 0)) {
+		av_free(avfc);
+		avfc = 0;
+		return;
+	}
+
+	avfc->max_delay= (int)(.7*AV_TIME_BASE);
+	av_opt_set_int(avfc, "preload", (int)(.5 * AV_TIME_BASE), AV_OPT_SEARCH_CHILDREN);
+	av_opt_set_int(avfc, "muxrate", 10080000, AV_OPT_SEARCH_CHILDREN);
+
+	av_dump_format(avfc, 0, filename, 1);
+	avformat_write_header(avfc, NULL);
+#else
   if ((av_set_parameters(avfc, NULL) < 0) || (!(fmt->flags & AVFMT_NOFILE)&&(url_fopen(&avfc->pb, filename, URL_WRONLY) < 0))) {
     av_free(avfc);
     avfc=0;
@@ -131,10 +176,10 @@ lavfmuxer::lavfmuxer(const char *format, uint32_t audiostreammask, mpgfile &mpg,
 
 
   dump_format(avfc, 0, filename, 1);
-  fileopened=true;
   av_write_header(avfc);
+#endif
+  fileopened=true;
   }
-
 
 lavfmuxer::~lavfmuxer()
   {
@@ -142,7 +187,9 @@ lavfmuxer::~lavfmuxer()
     if (fileopened) {
       av_write_trailer(avfc);
       if (!(fmt->flags & AVFMT_NOFILE))
-#if LIBAVFORMAT_VERSION_INT >= ((52<<16)+(0<<8)+0)
+#if LIBAVFORMAT_VERSION_INT >= ((53<<16)+(0<<8)+0)
+        avio_close(avfc->pb);
+#elif LIBAVFORMAT_VERSION_INT >= ((52<<16)+(0<<8)+0)
         url_fclose(avfc->pb);
 #else
         url_fclose(&avfc->pb);
